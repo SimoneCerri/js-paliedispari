2155eb496cb6b81e0e940a4bae26bc091d689cc1
SimoneCerri <iosonosimone@hotmail.it>
2024-02-27 15:54:06 +0100

    create a function for sumNumbers and for identify if the result is even or odd, add prompt to ask the user number and even\odd

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
-//Palindrome:
+//### Palindrome: ###
 
 //ask user to insert a word.
 let userWord = prompt("Give me a word please ! ^^");
-console.log(userWord);
+//console.log(userWord);
 
 //function () { } with IF inside to understand if word is palindrome.
 let long = userWord.length;
@@ -25,4 +25,37 @@ function checkPalindrome(userWord)
 /* console.log(checkPalindrome(userWord)); */
 let prova = checkPalindrome(userWord);
 
+
+//### Even or Odd: ###
+
+//ask user to choose even or odd.
+let userChoose = prompt("Choose between EVEN or ODD !");
+console.log(userChoose);
+
+//ask user number between 1 and 5.
+let userNumber = Number(prompt("Choose a number between 1 and 5"));
+console.log(userNumber);
+
+//mathrandom between 1 and 5 for npc number.
+let npcNumber = Math.round(Math.random() * (5 - 1) + 1);
+console.log(npcNumber);
+
+//sum the two number(userNumber + npcNumber).
+function sumNumbers ()
+{
+    let sum = npcNumber + userNumber;
+    return sum ;
+};
+console.log(sumNumbers(npcNumber,userNumber));
+
+
+//understand IF sum is even or odd.
+let sumNumbersVar = sumNumbers(npcNumber,userNumber);
+function isEven(sumNumbersVar)
+{
+    return (sumNumbersVar % 2 == 0);
+};
+isEven(sumNumbersVar) ? console.log("EVEN") : console.log("ODD");
+
+//ALERT with winner.
 
